8a3969dadb19b28a153fcf85dedbdc8c953efc47
Ankita Hazra <ankitahazra2000@gmail.com>
2022-10-06 11:36:32 +0530

    Create Weather Observation Station 15.sql

---
+++ Aggregation/Weather Observation Station 15.sql
@@ -0,0 +1,7 @@
+/*
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to  4 decimal places.
+*/
+select round((long_w),4)
+from station
+where (lat_n)<137.2345
+order by lat_n desc limit 1;
